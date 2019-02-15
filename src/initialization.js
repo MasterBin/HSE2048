a62b4372c985370ccb48c3fffd3f1ac66c9242bb
Nikita Samoylov <nasamoylov_1@edu.hse.ru>
2019-02-15 16:06:18 +0300

    add sprites

--- src/initialization.js
+++ src/initialization.js
@@ -1,14 +1,22 @@
 import MainScene from './main_scene.js';
 
 const config = {
-    width: 480,
-    height: 640,
+    width: 500,
+    height: 500,
     type: Phaser.AUTO,
     backgroundColor: 0x000000,
     physics: Phaser.ARCADE,
-    scene: MainScene 
+    scene:  MainScene 
 };
+
+function resize() {
+    //TODO:
+}
 
 window.onload = function () {
     var game = new Phaser.Game(config);
+    window.focus();
+    resize();
+    window.addEventListener("resize", resize, false);
 };
+
--- src/main_scene.js
+++ src/main_scene.js
@@ -1,28 +1,15 @@
 export default class MainScene extends Phaser.Scene {
     constructor() {
-        super("mainScene");
+        super("MainScene");
     }
 	preload() {
-        this.load.image('ball', 'assets/ball.png');
-		this.load.image('hills',"assets/hills.png");
+        this.load.image('tile', 'assets/minisquare.png');
+        this.load.image('field',"assets/field.png");
 	}
 	create() {
-        let background = this.add.image(256,320,'hills').setScale(1,1);
-         ball = this.add.image(50, 50, 'ball');
-        //ball.body.gravity.y = 0;
-		
+        let field = this.add.image(250,250,'field');
+        ball = this.add.image(65, 68, 'tile');
     }
-	update() {
-
-        if (ball.y >= 256 || ball.y <= 0) {
-            ball.x -= Math.random()*30;
-        }
-        if (ball.x >= 640  || ball.x <= 0) {
-            ball.x -= Math.random()*30;
-        }
-        
-        ++ball.x;
-        ++ball.y;
-	}
+	//update() {}
 }
 let ball;
